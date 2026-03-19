d86612e0bf7f9b90d6dced970e84bfea02ac97fd
Yurii Kleban <yurakleban@gmail.com>
2026-03-19 10:09:28 +0200

    Initial commit

---
+++ Task0/Task0/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task0
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+
+        public Book(string title, string author, int pages)
+        {
+            
+        }
+    }
+}
